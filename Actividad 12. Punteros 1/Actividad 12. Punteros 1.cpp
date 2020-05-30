dbb5fa6023d5c9a3159373ffb4377dc0fe147fe1
Alzero7 <jessar.alzero.7@gmail.com>
2020-05-30 01:11:59 -0500

    Actividad 10

--- Actividad 12. Punteros 1/Actividad 12. Punteros 1.cpp
+++ Actividad 12. Punteros 1/Actividad 12. Punteros 1.cpp
@@ -19,13 +19,17 @@ struct Informacion_Estudiante{
 	
 typedef struct Informacion_Estudiante  Datos;	
 
+char Datos_del_Estudiante(Datos *p);
+
+int Fecha_Nacimiento(Datos *m);
+
+char Datos_Profesionales(Datos *n);
+
 
 int main()
 {
 	int Answer=0;
-	int ID=0;
 	int i=0;
-	int op=0;
 	
 	printf("\n\t Ingrese el numero de personas a capturar: ");
 	scanf("%d",&Answer);
@@ -37,107 +41,11 @@ int main()
 		system("cls");
 		fflush(stdin);
 		
-		printf("\n\t Ingrese su ID: ");
-		gets(Registros[i].ID_estudiante);
-				
-		system("cls");
-		fflush(stdin);
+		Datos_del_Estudiante(&Registros[i]);
 		
-		printf("\n\t Ingrese su Nombre: ");
-		gets(Registros[i].Nombre);
+		Fecha_Nacimiento(&Registros[i]);
 		
-		system("cls");
-		fflush(stdin);
-		
-		printf("\n\t Ingrese su Apellido Paterno: ");
-		gets(Registros[i].Ap_Pat);
-		
-		system("cls");
-		fflush(stdin);
-		
-		printf("\n\t Ingrese su Apellido Materno: ");
-		gets(Registros[i].Ap_Mat);
-		
-		system("cls");
-		fflush(stdin);
-		
-		do
-		{
-			printf("\n\t Ingrese su Mes de Nacimiento: ");
-			scanf("%d",&Registros[i].Mes);
-	
-			system("cls");
-			fflush(stdin);
-		}while(Registros[i].Mes<0 || Registros[i].Mes>12);
-		
-		op=0;
-		do
-		{
-			printf("\n\t Ingrese su Dia de Nacimiento: ");
-			scanf("%d",&Registros[i].Dia);
-	
-			system("cls");
-			fflush(stdin);
-			
-			if((Registros[i].Mes==1 || Registros[i].Mes==3 || Registros[i].Mes==5 || Registros[i].Mes==7 || Registros[i].Mes==8 || Registros[i].Mes==10 || Registros[i].Mes==12) && (Registros[i].Dia>0 || Registros[i].Dia<=31))
-			{
-				op=1;
-			}
-			else
-			{
-				if((Registros[i].Mes==4 || Registros[i].Mes==6 || Registros[i].Mes==9 || Registros[i].Mes==11) && (Registros[i].Dia>0 || Registros[i].Dia<=30))
-				{
-					op=1;
-				}
-				else
-				{
-					if(Registros[i].Mes==2 && Registros[i].Dia==28)
-					{
-						op=1;
-					}
-				}
-			}
-		}while(op!=1);
-		
-		op=0;
-		do
-		{
-			printf("\n\t Ingrese su A%co de Nacimiento: ",164);
-			scanf("%d",&Registros[i].Year);
-	
-			system("cls");
-			fflush(stdin);
-			
-			if(Registros[i].Year<1950 || Registros[i].Year>2019)
-			{
-				op=1;
-			}
-		}while(op==1);
-		
-		
-		printf("\n\t Ingrese su Facultad: ");
-		gets(Registros[i].Facultad);
-		
-		system("cls");
-		fflush(stdin);
-		
-		printf("\n\t Ingrese su Carrera: ");
-		gets(Registros[i].Carrera);
-		
-		system("cls");
-		fflush(stdin);
-		
-		printf("\n\t Ingrese su Semestre: ");
-		gets(Registros[i].Semetre);
-		
-		system("cls");
-		fflush(stdin);
-		
-		printf("\n\t Ingrese su Genero: ");
-		gets(Registros[i].Genero);
-		
-		system("cls");
-		fflush(stdin);
+		Datos_Profesionales(&Registros[i]);
 		
 		if(i>0)
 		{
@@ -188,8 +96,127 @@ int main()
 		}
 	}
 	
-	
-	
 		
 	return 0;
 }
+
+
+char Datos_del_Estudiante(Datos *p)
+{
+	
+		printf("\n\t Ingrese su ID: ");
+		gets(p->ID_estudiante);
+		
+		system("cls");
+		fflush(stdin);
+		
+		printf("\n\t Ingrese su Nombre: ");
+		gets(p->Nombre);
+		
+		system("cls");
+		fflush(stdin);
+		
+		printf("\n\t Ingrese su Apellido Paterno: ");
+		gets(p->Ap_Pat);
+		
+		system("cls");
+		fflush(stdin);
+		
+		printf("\n\t Ingrese su Apellido Materno: ");
+		gets(p->Ap_Mat);
+		
+		system("cls");
+		fflush(stdin);
+		
+	return 0;
+}
+
+int Fecha_Nacimiento(Datos *m)
+{
+	int op=0;
+	do
+	{
+		printf("\n\t Ingrese su Mes de Nacimiento: ");
+		scanf("%d",&m->Mes);
+
+		system("cls");
+		fflush(stdin);
+	}while(m->Mes<0 || m->Mes>12);
+	
+	op=0;
+	do
+	{
+		printf("\n\t Ingrese su Dia de Nacimiento: ");
+		scanf("%d",&m->Dia);
+
+		system("cls");
+		fflush(stdin);
+		
+		if((m->Mes==1 || m->Mes==3 || m->Mes==5 || m->Mes==7 || m->Mes==8 || m->Mes==10 || m->Mes==12) && (m->Dia>0 || m->Dia<=31))
+		{
+			op=1;
+		}
+		else
+		{
+			if((m->Mes==4 || m->Mes==6 || m->Mes==9 || m->Mes==11) && (m->Dia>0 || m->Dia<=30))
+			{
+				op=1;
+			}
+			else
+			{
+				if(m->Mes==2 && m->Dia==28)
+				{
+					op=1;
+				}
+			}
+		}
+	}while(op!=1);
+	
+	op=0;
+	do
+	{
+		printf("\n\t Ingrese su A%co de Nacimiento: ",164);
+		scanf("%d",&m->Year);
+
+		system("cls");
+		fflush(stdin);
+		
+		if(m->Year<1950 || m->Year>2019)
+		{
+			op=1;
+		}
+	}while(op==1);
+		
+	return 0;
+}
+
+char Datos_Profesionales(Datos *n)
+{
+	printf("\n\t Ingrese su Facultad: ");
+	gets(n->Facultad);
+	
+	system("cls");
+	fflush(stdin);
+	
+	printf("\n\t Ingrese su Carrera: ");
+	gets(n->Carrera);
+	
+	system("cls");
+	fflush(stdin);
+	
+	printf("\n\t Ingrese su Semestre: ");
+	gets(n->Semetre);
+	
+	system("cls");
+	fflush(stdin);
+	
+	printf("\n\t Ingrese su Genero: ");
+	gets(n->Genero);
+	
+	system("cls");
+	fflush(stdin);
+	
+	return 0;
+}
+
+
